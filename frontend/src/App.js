9de684235d8d8553a9103d35462c96601dcf7871
Germán <german.escobarc@gmail.com>
2020-05-09 18:19:56 -0500

    Upload first version of frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState, useEffect, createRef} from 'react'
+import { Map, TileLayer } from 'react-leaflet'
+import Marker from './Marker'
+import Form from './Form';
+import incidentsService from './services/cases';
+import './App.css';
+
+const App = () => {
+  const [position, setPosition] = useState({ lat: 6.2486, lng: -75.5742, zoom: 13 })
+  const [incidents, setIncidents] = useState([])
+  const [form, setForm] = useState({ show: false })
+
+  useEffect(() => {
+    async function load() {
+      setIncidents(await incidentsService.list())
+    }
+
+    load()
+  }, [incidents])
+
+  const handleClick = e => {
+    console.log(e);
+    setForm({ show: true, lat: e.latlng.lat, lng: e.latlng.lng })
+  }
+
+  const handleCaseReported = incident => {
+    incident.latitude = form.lat;
+    incident.longitude = form.lng;
+
+    setIncidents(incidents.concat(incident))
+    setForm({ show: false })
+  }
+
+  const handleFormClose = ()  => {
+    setForm({ show: false })
+  }
+
+  const mapRef = createRef();
+  return (
+    <div>
+      <Map center={[position.lat, position.lng]} zoom={position.zoom} ref={mapRef} onClick={handleClick}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        { incidents.map(c => <Marker incident={c} key={`marker-${c.id}`} />) }
+      </Map>
+      { form.show ? <Form onCaseReported={handleCaseReported} onClose={handleFormClose} /> : null }
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render, waitFor } from '@testing-library/react'
+import App from './App'
+
+test('renders the map', async () => {
+  const { container } = render(<App />)
+  await waitFor(() => {
+    expect(container.querySelector('.leaflet-container')).toBeInTheDocument()
+  })
+
+})

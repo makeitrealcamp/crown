88a041d0dd97ac9c7af4f3f58709f11a55434f03
mardiniii <sezama03@hotmail.com>
2020-05-11 09:18:34 -0400

    Define registration(create user) and login(auth) routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      resources :users, param: "username", only: %i(create show)
       resources :incidents, only: %i(create show index)
+
+      post '/auth/login', to: 'authentication#login'
     end
   end
 end

dae2bcd8de57ef08af803d388bf5c8de1864d578
mardiniii <sezama03@hotmail.com>
2020-05-13 08:01:03 -0400

    Create the `Api::V1::SessionsController` class

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'sessions/create'
+    end
+  end
+  namespace :api do
+    namespace :v1 do
       resources :users, param: "username", only: %i(create show)
       resources :incidents, only: %i(create show index)
-
-      post '/auth/login', to: 'authentication#login'
+      resources :sessions,  only: %i(create)
     end
   end
 end

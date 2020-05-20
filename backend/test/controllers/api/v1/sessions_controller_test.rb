cbcc53a015d465f1551ba171a050a4691d2f5857
jairorjuela <genius85862@gmail.com>
2020-05-20 02:07:23 -0500

    Delete minitest files

--- backend/test/controllers/api/v1/sessions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class Api::V1::SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    params = { identifier: "jhonny-bravo", password: "crown-tracker" }
-    post api_v1_sessions_url, params: params
-    assert_response :success
-  end
-end

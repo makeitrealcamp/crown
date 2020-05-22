d82cf9d8b69a0fa6c758ba4b3a1e8dca663a537d
Shymmy Garcia <shymmy.garcia-ext@block.one>
2020-05-21 21:12:07 -0500

    Refactor login form

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,35 +1,55 @@
-import React, { useState } from 'react'
-
+import React from 'react'
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+} from 'formik'
+import * as Yup from 'yup'
+import TextError from '../utils/TextError'
 import authService from '../services/auth'
 
+
+const initialValues = {
+  username: '',
+  password: '',
+}
+
+const validationSchema = Yup.object({
+  username: Yup.string().required('This field is required'),
+  password: Yup.string().required('This field is required'),
+})
 const Login = ({ close }) => {
-  const [state, setState] = useState({ identifier: '', password: '' })
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    await authService.login(state.identifier, state.password)
-    close()
-  }
-
   return (
-    <div className="form-page auth-page">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username or Email:</label>
-          <input type="text" id="username" className="form-control" value={state.identifier} onChange={(e) => setState({ ...state, identifier: e.target.value })} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" className="form-control" value={state.password} onChange={(e) => setState({ ...state, password: e.target.value })} />
-        </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={async (values) => {
+        await authService.login(values.username, values.password)
+        close()
+      }}
+    >
+      <div className="form-page auth-page">
+        <Form className="form">
+          <div className="form-group">
+            <label htmlFor="username">Username or Email:</label>
+            <Field type="text" id="username" name="username" className="form-control" />
+            <ErrorMessage name="username" component={TextError} />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password:</label>
+            <Field type="password" id="password" name="password" className="form-control" />
+            <ErrorMessage name="password" component={TextError} />
+          </div>
 
-        <div className="actions">
-          <button type="button" onClick={() => close('incident')}>Cerrar</button>
-          <button type="submit">Login</button>
-        </div>
-      </form>
-    </div>
+          <div className="actions">
+            <button type="button" onClick={() => close('incident')}>Close</button>
+            <button type="submit">Login</button>
+          </div>
+
+        </Form>
+      </div>
+    </Formik>
   )
 }
 
---
+++ frontend/src/utils/TextError.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function TextError({ children }) {
+  return <div className="error">{children}</div>
+}
+
+export default TextError
